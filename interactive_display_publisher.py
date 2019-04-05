9b0b80f49fe6e95808eaa9fc0a558f5341fab6a4
wmharder <wmharder@ksu.edu>
2019-04-05 01:23:10 -0500

    reduce SPI rate to 1kHz

--- interactive_display_publisher.py
+++ interactive_display_publisher.py
@@ -49,7 +49,7 @@ def main():
     spi.open(0, 0) #Not really sure what this should be
 
     #Both of these come from section 4.1.2: http://ww1.microchip.com/downloads/en/DeviceDoc/Atmel-9570-AT42-QTouch-BSW-AT42QT1110-Automotive_Datasheet.pdf
-    spi.max_speed_hz = 14000000 #Max is 1.5 MHz, limit to 1.4 MHz to be safe
+    spi.max_speed_hz = 1000 #Max is 1.5 MHz, limit to 1.4 MHz to be safe
     spi.mode = 0b11 # Clock polarity = 1, Clock phase = 1
 
     tc = touch_controller()
@@ -206,8 +206,8 @@ class selection_manager(object):
             #While selection is above current selection
             while new_touch_ic > self.touch_ic:
                 self.increment()
-            print("Selected {}".format(new_touch_ic))
-            raw_input()
+            #print("Selected {}".format(new_touch_ic))
+            #raw_input()
             return True
 
         #If selection is invalid
