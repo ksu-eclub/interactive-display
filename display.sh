06bbef7142103952f818fd67d2e603502be1a24e
Zach Deibert <zachdeibert@gmail.com>
2019-04-05 04:04:20 -0500

    Fixed display script exiting

--- display.sh
+++ display.sh
@@ -5,6 +5,8 @@ Xvfb :1 -screen 0 1x1x8 &
 export DISPLAY=:1
 sleep 2
 export $(dbus-launch)
-pulseaudio &
-sleep 2
-pactl set-sink-volume 0 115%
+(
+    sleep 10s
+    pactl set-sink-volume 0 115%
+) &
+pulseaudio
