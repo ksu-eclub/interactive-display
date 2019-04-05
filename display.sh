a629d1c6c7dd658ecc725f830b0e2fc0f9e79a59
Zach Deibert <zachdeibert@gmail.com>
2019-04-05 04:01:45 -0500

    Updated display settings

--- display.sh
+++ display.sh
@@ -3,8 +3,8 @@ set -e
 
 Xvfb :1 -screen 0 1x1x8 &
 export DISPLAY=:1
-sleep 1
+sleep 2
 export $(dbus-launch)
 pulseaudio &
-sleep 1
-amixer -D pulse sset Master 100%
+sleep 2
+pactl set-sink-volume 0 115%
