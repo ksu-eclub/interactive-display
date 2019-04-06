3c6235d825adba18881a1d82adbcf679d918713a
Zach Deibert <zachdeibert@gmail.com>
2019-04-05 21:16:37 -0500

    Updated testing data

--- data/tonedata.go
+++ data/tonedata.go
@@ -3,7 +3,13 @@ package data
 var (
 	Tones = []toneMapping{
 		// touch channel, audio note, velocity
-		toneMap(97, 60, 100),
-		toneMap(98, 61, 100),
+		toneMap(98, 59, 100),
+		toneMap(97, 64, 100),
+		toneMap(89, 67, 100),
+		toneMap(90, 66, 100),
+		toneMap(109, 64, 100),
+		toneMap(108, 71, 100),
+		toneMap(102, 69, 100),
+		toneMap(103, 66, 100),
 	}
 )
